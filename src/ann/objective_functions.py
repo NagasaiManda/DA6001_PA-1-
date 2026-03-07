ef539ace7f897dfc4f0722c5cc525a59f6e19c06
NagasaiManda <mandanagasai@gmail.com>
2026-03-07 23:56:42 +0530

    last

--- src/ann/objective_functions.py
+++ src/ann/objective_functions.py
@@ -21,23 +21,24 @@ class MSE:
         return np.mean((y_preds - y_true)**2)
 
     __call__ = forward
-    def backward(self, y_true=None, y_preds=None):
-        return 2 * (self.y_preds - self.y_true) / self.y_preds.size
+    def backward(self, y_true, y_preds):
+        return 2 * (y_preds - y_true) / y_preds.size
 
 
 
 class CrossEntropyWithSoftmax:
     def forward(self, logits, y_true):
         self.y_true = y_true
-        logits_shift = logits - np.max(logits, axis=1, keepdims=True)
-        exp_logits = np.exp(logits_shift)
+        logits = logits - np.max(logits, axis=1, keepdims=True)
+        exp_logits = np.exp(logits)
         self.y_preds = exp_logits / np.sum(exp_logits, axis=1, keepdims=True)
-        logsumexp = np.log(np.sum(exp_logits, axis=1, keepdims=True))
-        log_probs = logits_shift - logsumexp
-        return -np.mean(np.sum(y_true * log_probs, axis=1))
+        return -np.mean(np.sum(y_true * np.log(self.y_preds), axis=1))
 
     __call__ = forward
-    def backward(self, y_true=None, logits=None):
-        return (self.y_preds - self.y_true) / self.y_preds.shape[0]
+    def backward(self, y_true, logits):
+        logits = logits - np.max(logits, axis=1, keepdims=True)
+        exp_logits = np.exp(logits)
+        y_preds = exp_logits / np.sum(exp_logits, axis=1, keepdims=True)
+        return (y_preds - y_true)/y_preds.shape[0]
 
     
